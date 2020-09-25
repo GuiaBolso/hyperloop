53c63d0e427ec4a09a1c7117e248c90d951026b9
José Victor <jose.victor@guiabolso.com.br>
2020-09-25 20:16:25 -0300

    Handling async transport properly

--- transport/src/main/kotlin/br/com/guiabolso/hyperloop/transport/aws/SQSTransport.kt
+++ transport/src/main/kotlin/br/com/guiabolso/hyperloop/transport/aws/SQSTransport.kt
@@ -1,6 +1,7 @@
 package br.com.guiabolso.hyperloop.transport.aws
 
-import br.com.guiabolso.hyperloop.transport.MessageResult
+import br.com.guiabolso.hyperloop.exceptions.SendMessageException
+import br.com.guiabolso.hyperloop.md5
 import br.com.guiabolso.hyperloop.transport.Transport
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
@@ -16,13 +17,14 @@ class SQSTransport(
         .withRegion(region)
         .build()
 
-    override fun sendMessage(message: String): MessageResult {
+    override fun sendMessage(message: String) {
         val sendMessageRequest = SendMessageRequest()
             .withQueueUrl(queueURL)
             .withMessageBody(message)
 
         val messageResult = sqs.sendMessage(sendMessageRequest)
-
-        return MessageResult(messageResult.messageId, messageResult.mD5OfMessageBody)
+        if (messageResult.mD5OfMessageBody != message.md5()) {
+            throw SendMessageException("Transport could not deliver message correctly! MD5 from event differs from MD5 of transport.")
+        }
     }
 }
--- transport/src/test/kotlin/br/com/guiabolso/hyperloop/HyperloopTest.kt
+++ transport/src/test/kotlin/br/com/guiabolso/hyperloop/HyperloopTest.kt
@@ -4,8 +4,6 @@ import br.com.guiabolso.events.builder.EventBuilder
 import br.com.guiabolso.events.model.RequestEvent
 import br.com.guiabolso.hyperloop.cryptography.cypher.MessageCypher
 import br.com.guiabolso.hyperloop.exceptions.InvalidInputException
-import br.com.guiabolso.hyperloop.exceptions.SendMessageException
-import br.com.guiabolso.hyperloop.transport.MessageResult
 import br.com.guiabolso.hyperloop.transport.Transport
 import br.com.guiabolso.hyperloop.util.Clock
 import br.com.guiabolso.hyperloop.util.isoFormat
@@ -32,7 +30,6 @@ class HyperloopTest {
     private var cryptographyEngine: MessageCypher = mock()
     private var clock: Clock = mock()
     private lateinit var event: RequestEvent
-    private lateinit var eventMD5: String
     private lateinit var date: LocalDateTime
     private val gson = Gson()
 
@@ -55,9 +52,7 @@ class HyperloopTest {
     @Test
     fun `test can send event as message`() {
         event.metadata["receivedAt"] = date.isoFormat()
-        eventMD5 = gson.toJson(event).md5()
         whenever(clock.now()).thenReturn(date)
-        whenever(transport.sendMessage(any())).thenReturn(MessageResult("some-id", eventMD5))
         whenever(validator.validate(any())).thenReturn(ValidationResult(true, mutableSetOf(), mutableSetOf()))
         whenever(cryptographyEngine.cypher(any())).thenAnswer {
             it.arguments[0] as String
@@ -68,22 +63,9 @@ class HyperloopTest {
         verify(transport).sendMessage(gson.toJson(event))
     }
 
-    @Test(expected = SendMessageException::class)
-    fun `test send event fails with invalid md5`() {
-        whenever(clock.now()).thenReturn(date)
-        whenever(transport.sendMessage(any())).thenReturn(MessageResult("some-id", "wrong-md5-hash"))
-        whenever(validator.validate(any())).thenReturn(ValidationResult(true, mutableSetOf(), mutableSetOf()))
-        whenever(cryptographyEngine.cypher(any())).thenAnswer {
-            it.arguments[0] as String
-        }
-
-        hyperloop.offer(event)
-    }
-
     @Test(expected = ValidationException::class)
     fun `test validate event fails`() {
         whenever(clock.now()).thenReturn(date)
-        whenever(transport.sendMessage(any())).thenReturn(MessageResult("some-id", "wrong-md5-hash"))
         whenever(validator.validate(any())).thenReturn(
             ValidationResult(
                 false,
