87f016bf178e3b70152a9cd013249c20d76930c6
Rafael Martins <59611629+rafael-guia@users.noreply.github.com>
2021-08-27 11:38:25 -0300

    Upgrade dependencies, language and plugin (#30)
    * Upgrade libs and jdk
    
    * Replacing depracted methods

--- transport/build.gradle.kts
+++ transport/build.gradle.kts
@@ -3,8 +3,8 @@ dependencies {
     implementation(project(":validator"))
 
     // AWS
-    implementation("com.amazonaws:aws-java-sdk-sqs:1.11.869")
-    implementation("com.amazonaws:aws-encryption-sdk-java:1.9.0")
+    implementation("com.amazonaws:aws-java-sdk-sqs:${rootProject.extra.get("aws-version")}")
+    implementation("com.amazonaws:aws-encryption-sdk-java:2.3.2")
 
     // Kotson
     implementation("com.github.salomonbrys.kotson:kotson:2.5.0")
